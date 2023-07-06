54fa862c99f9806a3db321d5ca3fa8907604b512
Hilal Karakurt <hllkrkrt27@gmail.com>
2023-07-06 18:07:09 +0300

    İkinci soru çözüldü.

--- project.py
+++ project.py
@@ -26,5 +26,25 @@ def list_flatten(liste):
 liste=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 print(list_flatten(liste))
 
+def list_reverse(liste):
+    global reversed_list
+    eleman=len(liste)
+    reversed_list=[]
+    a_list=[]
+    for i in(range(eleman)):
+        a_list=liste[i]
+        if type(a_list)== list:
+            a_list.reverse()
+            reversed_list.append(a_list)
+        else:
+            reversed_list.append(a_list)
+    reversed_list.reverse()
+    return reversed_list
+            
+  
+liste=[[1, 2], [3, 4], [5, 6, 7]]
+new_list=list_reverse(liste)
+print(new_list)
+
 
 
